aade6459c84a6ef4e02206637e5423a733b272d3
Simon Lundström <simlu+github@su.se>
2019-12-12 15:56:28 +0100

    Check that we get a username
    Otherwise it's an invalid request

--- caching_ldap/v1.py
+++ caching_ldap/v1.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 # pylint: disable=R,C
-from flask import Blueprint
+from flask import Blueprint, abort
+from flask import current_app as app
+from flask import request
 
 api = Blueprint("api", __name__, url_prefix="/v1")
 
 
 @api.route('/login', methods=['POST'])
 def login():
-    return {'great': "success"}, 200
+    if "username" in request.form:
+        return {'great': "success"}, 200
+    else:
+        abort(400)
