1b79eca7deff20d97830cc3d991d84a667ad7d3c
Simon Lundström <simlu+github@su.se>
2019-12-16 16:17:02 +0100

    Add TODO

--- caching_ldap/__init__.py
+++ caching_ldap/__init__.py
@@ -8,6 +8,18 @@ from caching_ldap.v1 import api as api_v1
 app = Flask(__name__)
 app.register_blueprint(api_v1)
 
+# TODO:
+# * cron which:
+#   * finds all driftansvariga-*-groups and add them to cached_users[uid]
+# * On /login
+#   * Get all groups from cached_users[uid]
+#   * Read the yaml config
+#   * Create list returned_acl
+#   * iterate over each group key e.g.
+#     'driftansvariga-configurationmanagement%':
+#     * if it exists in cached_users[uid]:
+#       * add it to a returned_acl
+#   * return returned_acl
 
 @app.errorhandler(werkzeug.exceptions.HTTPException)
 def handle_http_errors(e):
