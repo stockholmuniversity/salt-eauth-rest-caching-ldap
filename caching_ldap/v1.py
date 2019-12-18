7b5215a35622a8b0720655d8a1e406e14dcf8283
Simon Lundström <simlu+github@su.se>
2019-12-18 10:21:52 +0100

    Check and return groups from cached_users
    Even though we should return ACLs not the groups but baby steps!

--- caching_ldap/v1.py
+++ caching_ldap/v1.py
@@ -2,7 +2,7 @@
 # pylint: disable=R,C
 from flask import Blueprint, abort
 from flask import current_app as app
-from flask import request
+from flask import jsonify, request
 
 api = Blueprint("api", __name__, url_prefix="/v1")
 
@@ -10,6 +10,11 @@ api = Blueprint("api", __name__, url_prefix="/v1")
 @api.route('/login', methods=['POST'])
 def login():
     if "username" in request.form:
-        return {'great': "success"}, 200
+        username = request.form["username"]
+        if username in app.cached_users:
+            app.logger.error(app.cached_users[username])
+            return jsonify(app.cached_users[username]), 200
+        else:
+            abort(401)
     else:
         abort(400)
